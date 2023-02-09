d5fb10fbab89563243e6fd673cb2bcef0786b82b
rayli <rayli@LAPTOP-K395GLJO>
2023-02-09 18:38:11 +0800

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OddNumbersDivisibleByFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i <= 300; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
